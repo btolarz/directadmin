0d68a102cfa08bd915ba3cc87d62e4dabe5332e6
Bogusław Tolarz <btolarz@gmail.com>
2015-05-25 16:31:36 +0200

    ff

---
+++ lib/generators/direct_admin/install_generator.rb
@@ -0,0 +1,16 @@
+module DirectAdmin
+
+
+
+    class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Creates directadmin initializer"
+      def copy_initializer
+        template 'direct_admin.rb', 'config/initializers/direct_admin.rb'
+      end
+    end
+
+
+end
